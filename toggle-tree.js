8b4322199c6f1534682ca27cd9e6577deaddf938
Cpaz <nicky.shadow99@gmail.com>
2021-12-03 19:50:33 -0500

    added keybinding for toggle

--- toggle-tree.js
+++ toggle-tree.js
@@ -1,13 +1,28 @@
-(function() {
+(function () {
+
+	function toggleView() {
+		var tree = document.querySelector("#tree-td");
+		if (tree) {
+			tree.style.display = tree.style.display == "none" ? "" : "none";
+			GMEdit_Splitter.syncMain();
+		} else {
+			console.error("Could not find resource tree view");
+		}
+	}
+
 	GMEdit.register("toggle-tree", {
-		init: function() {
-			var tree = document.querySelector("#tree-td");
-			if (tree) $gmedit["ui.CommandPalette"].add({
-				name: "Toggle treeview",
-				exec: function() {
-					tree.style.display = tree.style.display == "none" ? "" : "none";
-					GMEdit_Splitter.syncMain();
+		init: function () {
+			AceCommands.add({
+				name: "toggleResourceView",
+				bindKey: "Ctrl-Shift-B",
+				exec: function (editor) {
+					toggleView();
 				}
+			});
+
+			AceCommands.addToPalette({
+				name: "Toggle Resource View",
+				exec: "toggleResourceView"
 			});
 		}
 	});
